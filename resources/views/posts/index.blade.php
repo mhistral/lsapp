7616d9b19e4e241c9dc40e21635eb51bfc0acac8
Kimberly Parrenas <kimberlyparrenas@gmail.com>
2020-01-23 14:19:54 +0800

    Some Commits

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app');
+
+@section('content')
+    <h1>Posts</h1>
+
+    @if (count($posts) > 1)
+        @foreach ($posts as $post)
+            <div class="well">
+                <h3>{{$post->title}}</h3>
+                <small>Written on {{$post->created_at}}</small>
+            </div>
+        @endforeach
+    @else
+        <p>No Posts Found</p>
+    @endif
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+/*
+Route::get('/hello', function () {
+    return "<h1>Hello<h1>";
 });
+
+Route::get('/users/{id}/{name}', function($id, $name) {
+    return 'This is '.$name.' with an ID of '.$id;
+});
+
+Route::get('/about', function() {
+    return view('pages.about');
+});
+*/
+
+Route::get('/', 'PagesController@index' );
+Route::get('/about', 'PagesController@about' );
+Route::get('/services', 'PagesController@services' );
+
+Route::resource('posts', 'PostsController');
