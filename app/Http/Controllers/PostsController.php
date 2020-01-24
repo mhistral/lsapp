f469fa500805a97a8a4f805e81dd3fc040fd1acf
Kimberly Parrenas <kimberlyparrenas@gmail.com>
2020-01-24 16:18:40 +0800

    after ck editor

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -53,6 +53,7 @@ class PostsController extends Controller
         $post =  new Post;
         $post->title =  $request->input('title');
         $post->body =  $request->input('body');
+        $post->user_id =  auth()->user()->id;
         $post->save();
 
         // with here indidcate the success nofication after you save it
@@ -80,7 +81,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -92,7 +94,19 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        //Create Post
+        $post = Post::find($id);
+        $post->title =  $request->input('title');
+        $post->body =  $request->input('body');
+        $post->save();
+
+        // with here indidcate the success nofication after you save it
+        return redirect('/posts')->with('success','Post Updated');   
     }
 
     /**
@@ -103,6 +117,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success','Post Removed');  
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <button type="button" class="btn btn-default">
-        <a href="/posts" class="btn btn-outline-secondary">Go Back</a>
-    </button>
+    <a href="/posts" class="btn btn-default">Go back</a>
     <h1>{{$post->title}}</h1>
 
     <div>
-        {{$post->body}}
+        {{-- This parse's the HTML tag --}}
+        {!!$post->body!!}
     </div>
 
     <hr>
     <small>Written on {{$post->created_at}} </small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
+
 
 @endsection
